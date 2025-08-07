82451c48029f49bec52c67cf1b75718939089d26
Firstname Lastname <hshah104@myseneca.ca>
2025-08-07 10:13:38 -0400

    Added grpassignment2.py with disk usage report

--- grpassignment2.py
+++ grpassignment2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
-from diskreport import get_disk_usage  # assuming your function is in diskreport.py
+from diskreport import get_disk_usage  # assuming your function is in diskrepor>
 
 def main():
     parser = argparse.ArgumentParser(description="System Reports and Metrics")
 
-    parser.add_argument('--disk', action='store_true', help='Show disk usage report')
+    parser.add_argument('--disk', action='store_true', help='Show disk usage re>
     
     args = parser.parse_args()
 
@@ -17,3 +17,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
